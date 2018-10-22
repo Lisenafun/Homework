6b933bb105380519cb97fefc8108d325fa3dbdc4
OlesiaViazankina <lisena.cmex@rambler.ru>
2018-10-22 15:33:49 +0300

    Урок 2. Выполнено 6 обязательных задач + 1.

--- src/lesson2/Lesson2.java
+++ src/lesson2/Lesson2.java
@@ -12,28 +12,28 @@ public class Lesson2 {
         int a = 15;
         int b = -3;
         int c = 6;
-        if (a>b & a>c & b>c) {
-            System.out.println( c + "," + b + "," + a);
-        } else if (b>a & b>c & a>c){
+        if (a > b & a > c & b > c) {
+            System.out.println(c + "," + b + "," + a);
+        } else if (b > a & b > c & a > c) {
             System.out.println(c + "," + a + "," + b);
-        } else if (c>a & c>b & b>a){
+        } else if (c > a & c > b & b > a) {
             System.out.println(a + "," + b + "," + c);
-        } else if (a>b & a>c & c>b){
+        } else if (a > b & a > c & c > b) {
             System.out.println(b + "," + c + "," + a);
-        } else if (b>a & b>c & c>a){
+        } else if (b > a & b > c & c > a) {
             System.out.println(a + "," + c + "," + b);
-        } else if (c>b & c>a & a>b){
+        } else if (c > b & c > a & a > b) {
             System.out.println(b + "," + a + "," + c);
         }
 /*      Задача 3*
         Создайте программу,
         выводящую на экран первые 20 элементов последовательности 2 4 8 16 32 64 128 ….
 */
-        System.out.println("Задача 2");
-        int i=2;
-        for (int j = 0; j < 20; j++){
-           System.out.println(i);
-            i*=2;
+        System.out.println("Задача 3");
+        int i = 2;
+        for (int j = 0; j < 20; j++) {
+            System.out.println(i);
+            i *= 2;
         }
 /*
         Задача 6*
@@ -44,21 +44,24 @@ public class Lesson2 {
         Трамвайное депо решило подарить сувенир обладателю каждого счастливого билета и теперь раздумывает, как много сувениров потребуется.
         С помощью программы подсчитайте сколько счастливых билетов в одном рулоне?
 */
-        System.out.println("Задача 3");
+        System.out.println("Задача 6");
         int sumLucky = 0;
         for (int d = 999999; d >= 1; d--) {
-            int sumFirst = (d - (d % 1000)) / 1000;
+//            int sumFirst = (d - (d % 1000)) / 1000;
+            int sumFirst = d / 1000;
             int sumSecond = d % 1000;
-            int firstNum = (sumFirst - (sumFirst % 100)) / 100;
+//            int firstNum = (sumFirst - (sumFirst % 100)) / 100;
+            int firstNum = sumFirst / 100;
             int secondNum = ((sumFirst - (sumFirst % 10)) / 10) % 10;
             int thirdNum = (int) (sumFirst - (((sumFirst - (sumFirst % 10)) / 10) / 0.1));
-            int fourNum = (sumSecond - (sumSecond % 100)) / 100;
+//            int fourNum = (sumSecond - (sumSecond % 100)) / 100;
+            int fourNum = sumSecond / 100;
             int fiveNum = ((sumSecond - (sumSecond % 10)) / 10) % 10;
             int sixNum = (int) (sumSecond - (((sumSecond - (sumSecond % 10)) / 10) / 0.1));
-                if (firstNum + secondNum + thirdNum == fourNum + fiveNum + sixNum){
-                    sumLucky++;
+            if (firstNum + secondNum + thirdNum == fourNum + fiveNum + sixNum) {
+                sumLucky++;
 //                    System.out.println(d + "," + sumLucky);
-                }
+            }
         }
         System.out.println("Количество счастливых билетов = " + sumLucky);
 /*      Задача 7*
@@ -67,13 +70,15 @@ public class Lesson2 {
         что слева от двоеточия показывается симметричная комбинация для той,
         что справа от двоеточия (например, 02:20, 11:11 или 15:51).
 */
-        System.out.println("Задача 4");
+        System.out.println("Задача 7");
         int luckyTime = 0;
         for (int h = 0; h <= 23; h++) {
-            int firstHour = (h - h % 10) / 10;
+//            int firstHour = (h - h % 10) / 10;
+            int firstHour = h / 10;
             int secHour = h % 10;
             for (int m = 0; m <= 59; m++) {
-                int firstMin = (m - m % 10) / 10;
+//                int firstMin = (m - m % 10) / 10;
+                int firstMin = m / 10;
                 int secMin = m % 10;
                 if (firstHour == secMin & secHour == firstMin) {
                     luckyTime++;
@@ -87,9 +92,9 @@ public class Lesson2 {
         Их координаты (x1, x2), (y1, y2), (z1, z2) заданы как целые числа.
         Нужно определить - является ли треугольник с заданными координатами прямоугольным.
 */
-        System.out.println("Задача 5");
+        System.out.println("Задача 8");
         int d = 3, d1 = 4, e = 3, e1 = 4, f = 1, f1 = 3;
-        if (d == f & f1 == e1 | d == e & d1 == f1 | d == e & e1 == f1){
+        if (d == f & f1 == e1 | d == e & d1 == f1 | d == e & e1 == f1) {
             System.out.println("Треугольник является прямоугольным");
 //        } else if (d1 == e1 == f1 | d == e == f){
 //            System.out.println("Это не треугольник, а линия");
@@ -103,9 +108,9 @@ public class Lesson2 {
         В цикле выводить сколько моющего средства осталось после мытья каждой тарелки
         В конце вывести, сколько тарелок осталось, когда моющее средство закончилось или наоборот.
 */
-        System.out.println("Задача 6");
+        System.out.println("Задача 9");
         double aosNum = 10;
-        for (int plateNum = 5; plateNum >= 0 ; plateNum--) {
+        for (int plateNum = 5; plateNum >= 0; plateNum--) {
             if (aosNum > 0) {
                 aosNum -= 0.5;
             } else if (aosNum == 0 & plateNum > 0) {
@@ -114,5 +119,24 @@ public class Lesson2 {
             }
         }
         System.out.println("Моющего средства осталось = " + aosNum);
+/*      Задача 2
+        Создайте программу,
+        выводящую на экран все неотрицательные элементы
+        последовательности 90 85 80 75 70 65 60 ….
+*/
+//        int k = 90;
+        System.out.println("Задача 2");
+        for (int j = 90; j >= 0; j -= 5) {
+            System.out.println(j);
+        }
+/*        Задача 4
+        Проверьте, является ли натуральное число — простым.*/
+        System.out.println("Задача 4");
+        int x = 5;
+        if (x>1 & x % 2 > 0 & x % 3 > 0 ){
+            System.out.println("Натуральное число " + x + " является простым");
+        } else {
+            System.out.println("Число " + x + " не является простым");
+        }
     }
 }
