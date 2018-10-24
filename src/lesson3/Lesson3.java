0305d1f4a576344319e7d9c5b3b3d2482b8b65db
OlesiaViazankina <lisena.cmex@rambler.ru>
2018-10-24 15:40:08 +0300

    Урок 3. Выполнено 2 задачи + почти выполнена 4

--- src/lesson3/Lesson3.java
+++ src/lesson3/Lesson3.java
@@ -60,36 +60,70 @@ public class Lesson3 {
     левой или правой, либо сообщить, что эти суммы модулей равны.
     Если пользователь введёт неподходящее число, то программа должна требовать
     повторного ввода до тех пор, пока не будет указано корректное значение.*/
-//        int [] arr4;
-//        for (int i = 0; i < 1 ; i++) {
-//            Scanner scanner = new Scanner(System.in);
-//            System.out.println("Введите любое целое положительное число ");
-//            int num = scanner.nextInt();
-//            if (num < 0 | num % 2 != 0) {
-//                System.out.println("Неверный ввод.");
-//                i--;
-//            } else if (num > 0 & num % 2 == 0) {
-//                arr4 = new int [num];
-//                for (int j = 0; j < arr4.length; j++) {
-//                    arr4[j] = j+2;
-//                }
-////                long f = System.currentTimeMillis();
-//                System.out.println(Arrays.toString(arr4));
-//            }
+        int [] arr4;
+        for (int i = 0; i < 1 ; i++) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите любое целое положительное число ");
+            int num = scanner.nextInt();
+            if (num < 0 | num % 2 != 0) {
+                System.out.println("Неверный ввод.");
+                i--;
+            } else if (num > 0 & num % 2 == 0) {
+                arr4 = new int [num];
+                int[] arr67 = new int[11];
+                for (int j = 0; j < arr67.length; j++) {
+                    arr67[j] = -5+j;
+                    int x;
+                    for (int k = 0; k < arr4.length ; k++) {
+                        x = (int) (Math.random() * 10);
+                        arr4[k] = arr67[x];
+                    }
+                }
+                System.out.println(Arrays.toString(arr4));
+                int num2 = num / 2;
+                int [] arr5 = new int[num2];
+                int [] arr6 = new int[num2];
+                System.arraycopy(arr4,0, arr5,0,num2);
+                System.arraycopy(arr4, num2, arr6,0,num2);
+                System.out.println(Arrays.toString(arr5) + Arrays.toString(arr6));
+                int sum1;
+                for (int j = 0; j < num2; j++) {
+                    int m = j+1;
+                    sum1 = Math.abs(arr5[j] + arr5[m]);
+                    System.out.println(sum1);
+                }
+            }
+        }
+
+//        int[] arr7 = new int[11];
+//        int[] arr67 = new int[5];
+//        int x;
+//        for (int i = 0; i < 11 ; i++) {
+//            arr7[i] = -5+i;
+//        }
+//        for (int i = 0; i < arr67.length ; i++) {
+//            x = (int) (Math.random() * 10);
+//            arr67[i] = arr7[x];
+//        }
+//        System.out.println(Arrays.toString(arr67));
+/*        Строки
+        1) Найти в строке указанную подстроку и заменить ее на новую.
+        Строку, ее подстроку для замены и новую подстроку вводит пользователь.*/
+//        Scanner inString = new Scanner(System.in);
+//        System.out.println("Введите строку в поле");
+//        String string1 = inString.next();
+//        String delString = inString.next();
+//        String copyString = inString.next();
+//        String [] string2 = string1;
+//        if (string1.contains(delString) == true){
 //
 //        }
 
-        int[] arr7 = new int[11];
-        int[] arr67 = new int[5];
-        int x;
-        for (int i = 0; i < 11 ; i++) {
-            arr7[i] = -5+i;
-        }
-        for (int i = 0; i < arr67.length ; i++) {
-            x = (int) (Math.random() * 10);
-            arr67[i] = arr7[x];
-        }
-        System.out.println(Arrays.toString(arr67));
+//        StringBuilder stringBuilder = new StringBuilder();
+//        stringBuilder.append(delString).append(copyString);
+//        System.out.println(stringBuilder.toString());
+
+
     }
 }
 
