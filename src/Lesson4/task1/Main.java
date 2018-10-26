278993b5ec29d6978d69e3515e56c04253799646
OlesiaViazankina <lisena.cmex@rambler.ru>
2018-10-26 16:28:58 +0300

    Урок 4. Выполнена 1 задача.

--- src/Lesson4/task1/Main.java
+++ src/Lesson4/task1/Main.java
@@ -2,13 +2,20 @@ package Lesson4.task1;
 
 public class Main {
     public static void main(String[] args) {
-        int TS;
         Triangle triangle = new Triangle();
-        Point point = new Point();
-//        point.setPoint();
-//        System.out.println(point.toString());
-        triangle.setTriangle();
-//        TS=triangle.squareT();
-        System.out.println("Площадь треугольника ="+triangle.squareT());
+        Rectangle rect = new Rectangle();
+        Circle circ = new Circle();
+//        triangle.setTriangle();
+//        triangle.squareT();
+//        triangle.perimetrT();
+//        System.out.println(triangle.toString());
+//        rect.setRec();
+//        rect.squareR();
+//        rect.perimetrR();
+//        System.out.println(rect.toString());
+//        circ.setCirc();
+//        circ.squareC();
+//        circ.lengthC();
+//        System.out.println(circ.toString());
     }
 }
