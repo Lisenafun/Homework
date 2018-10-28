1af5ee25989ade972eca40f484af10ff12e2e0bc
OlesiaViazankina <lisena.cmex@rambler.ru>
2018-10-28 23:13:33 +0300

    Урок 3. Выполнена 4 задача

--- src/lesson3/Lesson3.java
+++ src/lesson3/Lesson3.java
@@ -75,7 +75,7 @@ public class Lesson3 {
                     arr67[j] = -5+j;
                     int x;
                     for (int k = 0; k < arr4.length ; k++) {
-                        x = (int) (Math.random() * 10);
+                        x = (int) (Math.random() * 11);
                         arr4[k] = arr67[x];
                     }
                 }
@@ -86,24 +86,22 @@ public class Lesson3 {
                 System.arraycopy(arr4,0, arr5,0,num2);
                 System.arraycopy(arr4, num2, arr6,0,num2);
                 System.out.println(Arrays.toString(arr5) + Arrays.toString(arr6));
-                int sum1;
+                int sum1 = 0;
+                int sum2 = 0;
                 for (int j = 0; j < num2; j++) {
-//                    int sum1 += Math.abs(arr5[j]);
-                }
+                sum1 += Math.abs(arr5[j]);
+                sum2 += Math.abs(arr6[j]);
+            }
+            if (sum1 > sum2){
+                System.out.println("Сумма модулей первой половины массива больше второй");
+            } else if(sum2 > sum1){
+                System.out.println("Сумма модулей второй половины массива больше первой");
+            }else{
+                System.out.println("Сумма модулей первой и второй половины массива равны");
+            }
             }
         }
-
-//        int[] arr7 = new int[11];
-//        int[] arr67 = new int[5];
-//        int x;
-//        for (int i = 0; i < 11 ; i++) {
-//            arr7[i] = -5+i;
-//        }
-//        for (int i = 0; i < arr67.length ; i++) {
-//            x = (int) (Math.random() * 10);
-//            arr67[i] = arr7[x];
-//        }
-//        System.out.println(Arrays.toString(arr67));
+        
 /*        Строки
         1) Найти в строке указанную подстроку и заменить ее на новую.
         Строку, ее подстроку для замены и новую подстроку вводит пользователь.*/
