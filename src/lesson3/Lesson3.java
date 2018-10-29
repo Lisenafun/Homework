d19aeabf912eaa719330cf910facc366da250505
OlesiaViazankina <lisena.cmex@rambler.ru>
2018-10-29 15:23:25 +0300

    Урок 3. Выполнена 4 задача + Строки 1 задача

--- src/lesson3/Lesson3.java
+++ src/lesson3/Lesson3.java
@@ -1,7 +1,11 @@
 package lesson3;
 
+import jdk.nashorn.internal.runtime.regexp.joni.Matcher;
+
+import javax.xml.bind.SchemaOutputResolver;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Lesson3 {
     public static void main(String[] args) {
@@ -10,7 +14,7 @@ public class Lesson3 {
    Создайте массив из всех чётных чисел от 2 до 20 и выведите элементы массива на экран сначала в строку,
    отделяя один элемент от другого пробелом, а затем в столбик (отделяя один элемент от другого началом новой строки).
    Перед созданием массива подумайте, какого он будет размера. 2 4 6 … 18 20 2 4 6 … 20
-*/
+*//*
         System.out.println("Задача 1");
         int [] arr = new int[10];
         for (int i = 2, b = 0; i <= 20; i++) {
@@ -29,10 +33,10 @@ public class Lesson3 {
                 b++;
             }
         }
-/*        Задача 2
+*//*        Задача 2
         Создайте массив из всех нечётных чисел от 1 до 99, выведите его на экран в строку,
         а затем этот же массив выведите на экран тоже в строку,
-        но в обратном порядке (99 97 95 93 … 7 5 3 1).*/
+        но в обратном порядке (99 97 95 93 … 7 5 3 1).*//*
 //        int a=0;
 //        for(int i=1;i<=99;i++){
 //            System.out.println(a);
@@ -52,14 +56,14 @@ public class Lesson3 {
             System.out.print(arr2[i] + " ");
         }
         System.out.println(" ");
-/*        Задача 4*
+*//*        Задача 4*
     Пользователь должен указать с клавиатуры чётное положительное число,
     а программа должна создать массив указанного размера из случайных целых чисел из [-5;5]
     и вывести его на экран в строку. После этого программа должна определить
     и сообщить пользователю о том, сумма модулей какой половины массива больше:
     левой или правой, либо сообщить, что эти суммы модулей равны.
     Если пользователь введёт неподходящее число, то программа должна требовать
-    повторного ввода до тех пор, пока не будет указано корректное значение.*/
+    повторного ввода до тех пор, пока не будет указано корректное значение.*//*
         int [] arr4;
         for (int i = 0; i < 1 ; i++) {
             Scanner scanner = new Scanner(System.in);
@@ -100,24 +104,24 @@ public class Lesson3 {
                 System.out.println("Сумма модулей первой и второй половины массива равны");
             }
             }
-        }
-        
+        }*/
+
 /*        Строки
         1) Найти в строке указанную подстроку и заменить ее на новую.
         Строку, ее подстроку для замены и новую подстроку вводит пользователь.*/
-//        Scanner inString = new Scanner(System.in);
-//        System.out.println("Введите строку в поле");
-//        String string1 = inString.next();
-//        String delString = inString.next();
-//        String copyString = inString.next();
-//        String [] string2 = string1;
-//        if (string1.contains(delString) == true){
-//
-//        }
+        Scanner inString = new Scanner(System.in);
+        System.out.print("Введите строку в поле:");
+        String string1 = inString.nextLine();
+        System.out.print("Введите часть строки, которую необходимо заменить:");
+        String delString = inString.nextLine();
+        System.out.print("Введите выражение, которое необходимо вставить:");
+        String copyString = inString.nextLine();
+        String stringNew = string1.replaceAll(delString,copyString);
+        System.out.println("Итого:" + stringNew);
+        stringNew.replaceAll("[^a-z]+","*");
+        System.out.println("Убираем повторяющиеся символы:" + stringNew);
+        System.out.println("Убираем пробелы:" + stringNew.replaceAll("\\s", ""));
 
-//        StringBuilder stringBuilder = new StringBuilder();
-//        stringBuilder.append(delString).append(copyString);
-//        System.out.println(stringBuilder.toString());
 
 
     }
