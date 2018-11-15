cae222af7e92f5c28a4ac29ded49d9d43fa327cd
OlesiaViazankina <lisena.cmex@rambler.ru>
2018-11-15 23:17:18 +0300

    Урок 11. Коллекции. Задача 1,2,3,5,6 сделано.          Синтаксис. Задачи 2.3.5.6 сделано.

---
+++ src/lesson11Col/task7/Task7.java
@@ -0,0 +1,13 @@
+package lesson11Col.task7;
+
+import java.util.Map;
+
+public class Task7 {
+    public static void main(String[] args) {
+    /*    Задача 7
+    Создать словарь (Map<String, String>) занести в него десять записей по принципу «Фамилия» - «Имя».
+    Проверить сколько людей имеют совпадающие с заданным именем или фамилией.
+    Результат вывести на экран*/
+        
+    }
+}
